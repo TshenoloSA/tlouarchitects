b192fc9b5aabdd5f2cbc400f25e5191a445cd6e0
TshenoloSA <93530208+TshenoloSA@users.noreply.github.com>
2024-10-27 01:18:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Simple script to scroll smoothly to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// JavaScript to toggle the navbar menu on small screens
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
